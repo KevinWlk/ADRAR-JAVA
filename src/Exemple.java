4def135c2094526982b8af9d77d3520ca11f9c61
KevinWlk <kevin.walencik@gmail.com>
2024-02-12 10:35:28 +0100

    init

---
+++ src/Exemple.java
@@ -0,0 +1,9 @@
+public class Exemple {
+    public String nom;
+    public String prenom;
+    public Exemple(){};
+    public Exemple(String lastname, String fistname){
+        this.nom = lastname;
+        this.prenom = fistname;
+    };
+}
